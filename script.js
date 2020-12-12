4ccc156ecaf1a9750ad1fcd193a01d078dad2841
sylvain0801 <treillardsylvain@hotmail.fr>
2020-12-12 10:33:52 +0100

    Ajout menu burger

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.navbar')
+const background = document.querySelector('.bg-image')
 
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+  nav.classList.toggle('active')
+})
+background.addEventListener('click', () => {
+  burger.classList.remove('active')
+  nav.classList.remove('active')
+})
